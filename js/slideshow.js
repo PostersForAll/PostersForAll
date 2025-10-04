ee94905020d20d49a38882fab26584a3be3b77e1
ichan70 <ichan70@stuy.edu>
2025-10-03 23:32:40 -0400

    cleaned code up

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,5 @@
 let slideIndex = 1;
-autoShowSlides();
+showSlides(1);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -10,48 +10,25 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
+  let item;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   
-  // Wrap around logic
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }    
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   
-  // Hide all slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  for (item = 0; item < slides.length; item++) {
+    slides[item].style.display = "none";  
   }
   
-  // Deactivate all dots
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (item = 0; item < dots.length; item++) {
+    dots[item].className = dots[item].className.replace(" active", "");
   }
   
-  // Show the current slide and activate the current dot
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-function autoShowSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1
-  }    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(autoShowSlides, 8000);
+  dots[slideIndex-1].className += " active"; /* 0_0 */
 }
